9accc6712806adae63478eea17ef910f8e081736
elingfathurrodo-efr <elingfathurrodo@gmail.com>
2026-03-11 12:33:40 +0700

    Create idea_generator.py

---
+++ brain/curiosity/idea_generator.py
@@ -0,0 +1,15 @@
+import random
+
+ideas=[
+
+"combine_rsi_ema",
+"modify_macd_speed",
+"add_volatility_filter",
+"use_breakout_logic",
+"mix_smc_strategy"
+
+]
+
+def generate_idea():
+
+    return random.choice(ideas)

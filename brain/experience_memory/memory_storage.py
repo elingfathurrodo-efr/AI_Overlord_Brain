75402b0715fb3315f685f148d845c146605136d1
elingfathurrodo-efr <elingfathurrodo@gmail.com>
2026-03-12 04:57:46 +0700

    Create memory_storage.py

---
+++ brain/experience_memory/memory_storage.py
@@ -0,0 +1,20 @@
+import json
+
+file="experience_memory.json"
+
+def load_memory():
+
+    try:
+
+        with open(file,"r") as f:
+            return json.load(f)
+
+    except:
+
+        return []
+
+
+def save_memory(data):
+
+    with open(file,"w") as f:
+        json.dump(data,f,indent=2)

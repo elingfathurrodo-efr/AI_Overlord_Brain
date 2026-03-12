496afd4060ed243e66a7c89928a96a4f2794b3ab
elingfathurrodo-efr <elingfathurrodo@gmail.com>
2026-03-12 11:30:15 +0700

    Create strategy_pool.py

---
+++ brain/strategy_fusion/strategy_pool.py
@@ -0,0 +1,15 @@
+def load_strategies():
+
+    strategies = [
+
+        {"name":"MA_RSI","type":"trend"},
+
+        {"name":"BOLLINGER_BREAKOUT","type":"volatility"},
+
+        {"name":"RSI_RANGE","type":"range"},
+
+        {"name":"MACD_TREND","type":"trend"}
+
+    ]
+
+    return strategies

c2f7a300df87a495c794b567796126ca2b8dc763
elingfathurrodo-efr <elingfathurrodo@gmail.com>
2026-03-12 11:06:59 +0700

    Create reflection_logger.py

---
+++ brain/self_reflection/reflection_logger.py
@@ -0,0 +1,19 @@
+import json
+
+file = "reflection_log.json"
+
+
+def load_log():
+
+    try:
+        with open(file,"r") as f:
+            return json.load(f)
+
+    except:
+        return []
+
+
+def save_log(data):
+
+    with open(file,"w") as f:
+        json.dump(data,f,indent=2)

77b0b6e49725e1be1f4188efd38b96ccad2b2cbf
elingfathurrodo-efr <elingfathurrodo@gmail.com>
2026-03-12 11:15:21 +0700

    Update parameter_tuner.py

--- brain/optimization_engine/parameter_tuner.py
+++ brain/optimization_engine/parameter_tuner.py
@@ -6,4 +6,8 @@ def tune_parameters(params):
 
     for key,value in params.items():
 
-        change = random.uniform(-0
+        change = random.uniform(-0.1,0.1)
+
+        new_params[key] = round(value + value*change,2)
+
+    return new_params

e807da99d4c7ecdf27dc069695e8fee8e4039a22
elingfathurrodo-efr <elingfathurrodo@gmail.com>
2026-03-12 11:56:11 +0700

    Create market_reader.py

---
+++ brain/central_brain/market_reader.py
@@ -0,0 +1,12 @@
+def read_market():
+
+    market_data = {
+
+        "symbol":"EURUSD",
+        "price":1.0850,
+        "volatility":"NORMAL",
+        "regime":"TREND"
+
+    }
+
+    return market_data
